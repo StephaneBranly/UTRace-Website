1b88af3f6277f725fb7f295e78d532c80ab2b3fe
StephaneBranly <stephanebranly.pro@gmail.com>
2020-03-11 18:07:45 +0100

    add: new ban available and credits

--- A_propos.php
+++ A_propos.php
@@ -26,7 +26,7 @@
                 <a class='link' href='https://github.com/StephaneBranly/UTRace-Website/issues' target='_blank'>ici<a> </p> 
                 <p>Police d'écriture : <a href='https://www.dafont.com/fr/nasalization.font' target='_blank' class='link'>Nazalisation</a></p>
                 <p>Icones : <a href='http://fontello.com/' target='_blank' class='link'>Fontello</a></p>
-                <p><i class='icone icone_margin'>&#xe835;</i>Graphismes par
+                <p><i class='icone icone_margin'>&#xe835;</i>Graphismes par <a class='link' href='https://www.instagram.com/_jeandieu_/' target='_blank'>Jeanne Parisse<a>, 
                 <a class='link' href='https://github.com/StephaneBranly' target='_blank'>Stéphane Branly<a> 
                     et <a class='link' href=''>Thibault Le Porho<a> </p> 
             </div>
