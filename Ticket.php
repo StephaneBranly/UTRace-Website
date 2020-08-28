0b69f253f53098bfe3852a7b581bfefa8242fab1
StephaneBranly <stephanebranly.pro@gmail.com>
2020-08-28 18:46:11 +0200

    add: transmission post form

--- Ticket.php
+++ Ticket.php
@@ -13,31 +13,59 @@
 	</head>
 	<body>
         <?php include_once("./elements/header.php"); ?>
-        <section>
-            <div class='content_section'>
-                <h1 class="section_name">Ticket tiercé généré</h1> 
-                <p>Ton ticket a bien été généré, n'oublie pas de le valider au stand en échange d'1€ symbolique, retiens le numéro # de ton ticket tiercé !</p><div id='ticket_section'>
-        <table cellspacing="0" cellpadding="0" id='ticket'>
-            <tr><td class='left' id='ticket_number'>
-                Ticket #00026
-            </td>
-            <td class='right' id='name'>Stéphane BRANLY
-            </td></tr>
-            <tr><td class='left'>
-                <p>Ticket à valider<br/>au stand</p>
-            </td><td class='right'>
-                <p id='tel'>Téléphone : 07-62-62-62-62</p>
-                <p id='email'>Mail : email@mail.com</p>
-                <p id='classement'>
-                    1er - Les barjos (equipe 1)<br/>
-                    2eme - Les bgs (equipe 6)<br/>
-                    3eme - Les tarés (equipe 4)
-                </p>
-            </td></tr>
-        </table>
-        </div>
-            </div>
-        </section>
+
+        <?php 
+            if(isset($_POST["submit"])){
+                if(isset($_POST["mail"])&&$_POST["mail"]!=""&&
+                isset($_POST["name"])&&$_POST["name"]!=""&&
+                isset($_POST["surname"])&&$_POST["surname"]!=""&&
+                isset($_POST["phone"])&&$_POST["phone"]!=""&&
+                isset($_POST["first"])&&
+                isset($_POST["second"])&&
+                isset($_POST["third"])){
+                    $name = addslashes($_POST["name"]);
+                    $surname = addslashes($_POST["surname"]);
+                    $mail = addslashes($_POST["mail"]);
+                    $phone = addslashes($_POST["phone"]);
+                    $first = addslashes($_POST["first"]);
+                    $second = addslashes($_POST["second"]);
+                    $third = addslashes($_POST["third"]);
+                    echo "<section>
+                            <div class='content_section'>
+                                <h1 class='section_name'>Ticket tiercé généré</h1> 
+                                <p>Ton ticket a bien été généré, n'oublie pas de le valider au stand en échange d'1€ symbolique, retiens le numéro # de ton ticket tiercé !</p>
+                                <div id='ticket_section'>
+                                    <table cellspacing='0' cellpadding='0' id='ticket'>
+                                        <tr>
+                                            <td class='left' id='ticket_number'>
+                                                Ticket #00026
+                                            </td>
+                                            <td class='right' id='name'>
+                                            $name $surname
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td class='left'>
+                                                <p>Ticket à valider<br/>au stand</p>
+                                            </td>
+                                            <td class='right'>
+                                                <p id='tel'>Téléphone : $phone</p>
+                                                <p id='email'>Mail : $mail</p>
+                                                <p id='classement'>
+                                                    1er - Les barjos (equipe $first)<br/>
+                                                    2eme - Les bgs (equipe $second)<br/>
+                                                    3eme - Les tarés (equipe $third)
+                                                </p>
+                                            </td>
+                                        </tr>
+                                    </table>
+                                </div>
+                            </div>
+                        </section>";
+                }
+            }
+        ?>
+       
         
         <?php include_once("./elements/footer.php"); ?>
     </body>
--- elements/header.php
+++ elements/header.php
@@ -4,8 +4,8 @@
         <ul>
             <li><a href='accueil'><i class='icone icone-home'>&#xe82c;</i>Accueil</a></li>
             <!--<li><a href='equipes'><i class='icone icon-users-2'>&#xe8be;</i>Les équipes</a></li>
-            <li><a href='vote'><i class='icone icon-users-2'>&#xe8be;</i>Noter</a></li>
-            <li><a href='tierce'><i class='icone'>&#xe81d;</i>Parier</a></li> -->
+            <li><a href='vote'><i class='icone icon-users-2'>&#xe8be;</i>Noter</a></li>-->
+            <li><a href='tierce'><i class='icone'>&#xe81d;</i>Parier</a></li>
             <li><a href='evenement'><i class='icone icon-award-1'>&#xe8b9;</i>L'événement</a></li>
            <!--<li><a href='association'><i class='icone icon-vcard'>&#xe8b3;</i>L'association</a></li>
             <li><a href='sponsors'><i class='icone icon-heart-1'>&#xe8a1;</i>Les sponsors</a></li>
